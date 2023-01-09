34a39e7784cc5007a38192b12f4cba3e568e429b
MiaChu <chusiying@outlook.com>
2023-01-10 00:58:23 +0800

    修正連線字串錯誤

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -1,15 +1,26 @@
 using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 using Microsoft.Extensions.Configuration;
+using Library.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddMvc();
+
+builder.Services.AddDbContext<LibraryContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryConnection")));
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddScoped<LibraryContext>();  
 var app = builder.Build();
 
-builder.Configuration.GetConnectionString("LibraryConnection");
+//builder.Configuration.GetConnectionString("LibraryConnection");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
